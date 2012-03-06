15d23578aba947effcb6dbfef6caf7774ea31a9b
Craig Odell <codell@ubuntWorkstation.(none)>
2012-03-05 22:50:21 -0700

    adding engine to the repo

---
+++ engine/AutoPerformanceClient.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 3, 2012
+
+@author: codell
+'''
+
+class AutoPerformanceClient(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
---
+++ engine/AutoPerformanceServer.py
@@ -0,0 +1,18 @@
+'''
+Created on Mar 3, 2012
+
+@author: codell
+'''
+
+class AutoPerformanceServer(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+        
