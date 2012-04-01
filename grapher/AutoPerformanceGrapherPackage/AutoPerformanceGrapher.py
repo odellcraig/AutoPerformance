555cca6d92c76fb5407411857927de9887eb14ea
Cody Hanson <codyhanson50@gmail.com>
2012-04-01 17:15:02 -0600

    started playing around with matplotlib, and reading in files into lists of data

--- grapher/AutoPerformanceGrapherPackage/AutoPerformanceGrapher.py
+++ grapher/AutoPerformanceGrapherPackage/AutoPerformanceGrapher.py
@@ -3,15 +3,129 @@ Created on Mar 5, 2012
 
 @author: cody
 '''
+import matplotlib.pyplot as plt
 
-class MyClass(object):
+
+'''
+base class for both datasets
+'''
+class dataset(object):
+    
+    def __init__(self,_type, _filename):
+        self.type = _type
+        #type should be 'udp', 'tcp'
+        
+        self.dates = []
+        
+        #'udp' type lists
+        self.udp_mbps = []
+        self.udp_losspercent = []
+        self.udp_jit = []
+        self.udp_dup = []
+        self.udp_reord = []
+        
+        #'tcp' type lists
+        self.tcp_mbps = []
+        self.tcp_rtt = []
+        self.tcp_jit = []
+        
+        self.filename = _filename
+        #read in the file line by line
+        fileIn = open(self.filename, 'r')
+        #if not fileIn:
+        #   print "Unable to open ", self.filename
+        first = True
+        for line in fileIn:
+            #skip the first line
+            if first == True:
+                first = False
+                continue
+            
+            tokens = line.split(',')
+            
+            self.dates.append(tokens[0])
+            
+            if self.type == 'udp':
+                self.udp_mbps.append(tokens[1])
+                self.udp_losspercent.append(tokens[2])
+                self.udp_jit.append(tokens[3])
+                self.udp_dup.append(tokens[4])
+                self.udp_reord.append(tokens[5])            
+
+            elif self.type == 'tcp':
+                self.tcp_mbps.append(tokens[1])
+                self.tcp_rtt.append(tokens[2])
+                self.tcp_jit.append(tokens[3])
+            else: #error
+                print 'bad type'
+                
+    def getUdpLists(self):
+        udpMap = {}
+        udpMap['time'] = self.dates
+        udpMap['mbps'] = self.udp_mbps
+        udpMap['jit'] = self.udp_jit
+        udpMap['dup'] = self.udp_dup
+        udpMap['reord'] = self.udp_reord
+        udpMap['loss'] = self.udp_losspercent
+        return udpMap
+
+
+
+
+
+'''
+Loads and allows access to records from a UDP test summary file format
+'''
+class UDPdataset(dataset):
+    
+    pass
+
+'''
+Loads and allows access to records from a TCP test summary file format
+'''
+class TCPdataset(dataset):
+    
+    pass
+
+
+
+class grapher(object):
     '''
     classdocs
     '''
 
 
-    def __init__(selfparams):
+    def __init__(self,_dataset):
         '''
         Constructor
         '''
+        self.dataset = _dataset 
+        self.title = "Default Graph Title"
+        self.xlabel = "X axis"
+        self.ylabel = "y axis"
         
+        #read in the file specified and parse the data
+
+        pass
+    
+    
+    def saveThroughput(self,_OutputFilename):
+        
+        pass
+    
+    def saveJitter(self,_OutputFilename):
+        
+        pass
+    
+    
+    def setTitle(self,title):
+        self.title
+        pass
+    
+    def setAxisX(self,_label):
+        self.ylabel = _label
+        pass
+    
+    def setXisY(self,_label):
+        self.ylabel = _label
+        pass
--- grapher/grapher.py
+++ grapher/grapher.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 
-plt.plot([2,2,2,2])
-plt.plot([1,2,3,4,5,6,7,8])
+plt.plot_date([2001,2001.5,2002,2003,2004,2005],[1,2,3,4,5,6],xdate=True)
 plt.ylabel('some numbers')
 plt.savefig("fig1")
 plt.show()
