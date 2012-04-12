cbf912e1aab278e2a1237142f96917c9350e5649
Cody Hanson <codyhanson50@gmail.com>
2012-04-12 11:43:44 -0600

    got some graphs coming out, still fighting on getting the axis and scaling correct

--- grapher/grapher.py
+++ grapher/grapher.py
@@ -2,5 +2,5 @@ import matplotlib.pyplot as plt
 
 plt.plot_date([2001,2001.5,2002,2003,2004,2005],[1,2,3,4,5,6],xdate=True)
 plt.ylabel('some numbers')
-plt.savefig("fig1")
+#plt.savefig("fig1")
 plt.show()
